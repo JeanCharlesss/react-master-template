8422a5074e22455b4aef65838a8efd65e8059ddf
Jean Charles <jean.fullstack01@gmail.com>
2023-07-16 17:38:36 -0300

    updating

--- src/components/Example/Style.jsx
+++ src/components/Example/Style.jsx
@@ -1,8 +1,14 @@
 import styled from "styled-components";
 
-export const ExampleWrapper = styled.div.attrs(({
-    className : 'bg-blue-500 text-white mt-[5px] mx-auto rounded text-center'
-}))`
+export const ExampleWrapper = styled.div.attrs(() => ({
+    className: `
+      bg-blue-500
+      text-white
+      mt-[5px]
+      mx-auto
+      rounded
+      text-center`,
+  }))`
     height: 150px;
     width: 300px;
 `;
